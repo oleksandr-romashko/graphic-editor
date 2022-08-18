e73cf67d2b786ae8fd9031cead6ac75d54b0f7d1
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-08-18 23:06:58 +0200

    Add shapes picker

---
+++ src/global/goit/romashko/graphic_editor/shape_picker/ShapePicker.java
@@ -0,0 +1,7 @@
+package global.goit.romashko.graphic_editor.shape_picker;
+
+public class ShapePicker {
+    public void printShapeName(Shape shape) {
+        System.out.println(shape.getName());
+    }
+}
